732ad00d2486599f8d68c785bd171bfc971fb629
Stephen <stephenkago10@gmail.com>
2022-08-28 02:41:29 +0300

    Completed ...Two Sum

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //iterate over the array
+for(let i = 0; i < array.length; i++){ 
+  // find a complementary number
+  const complement = target - array[i];
+  // iterate the remaining numbers
+  for (let j = i +1; j < array.length; j++){
+    // check if the the remaining numbers matches the complement
+    if (array[j] === complement) return true;
+  } 
+} return false;
 }
 
 /* 
@@ -32,3 +42,20 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+
+//////////////////Alternatively/////////////////////
+// function hasTargetSum(array, target) {
+//   // 1 step
+//   const seenNumbers = {};
+//   for (const number of array) {
+//     // n steps
+//     const complement = target - number;
+//     // n steps
+//     if (seenNumbers[complement]) return true;
+//     // n steps
+//     seenNumbers[number] = true;
+//   }
+//   // 1 step
+//   return false;
+// }
